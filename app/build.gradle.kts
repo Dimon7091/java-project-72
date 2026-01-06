a07042fb1c2595007149511646a1e0a551b2d897
Dmitry Gorbunov <dimarik70rus@gmail.com>
2026-01-06 15:58:12 +0700

    add a Dockerfile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,31 @@
+import gg.jte.ContentType
+
 plugins {
-    id("java")
+    kotlin("jvm") version "1.9.20"
+    application
+    id("com.gradleup.shadow") version "9.0.0"
+    id("gg.jte.gradle") version "3.2.1"
 }
+
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App");
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("gg.jte:jte:3.2.1")
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("io.javalin:javalin-rendering:6.7.0")
+
+    implementation("ch.qos.logback:logback-classic:1.5.6")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -18,4 +34,9 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+jte {
+    sourceDirectory.set(project.file("src/main/resources/templates").toPath())
+    contentType.set(ContentType.Html)
 }
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,48 @@
 package hexlet.code;
 
-public class App {
-    public static void main(String[] args) {
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+import io.javalin.rendering.template.JavalinJte;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 
+public class App {
+    private static final Logger log = LoggerFactory.getLogger(App.class);
+    public static void main(String[] args) {
+        getApp();
+    }
+
+    public static Javalin getApp() {
+        var codeResolver   = new ResourceCodeResolver("templates");
+        var templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte(templateEngine));
+            config.staticFiles.add("static", Location.CLASSPATH);
+        });
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+        var port = getPort();
+        app.start(port);
+        return app;
+    }
+
+    private static int getPort() {
+        // 1. JVM property (из Dockerfile)
+        String propPort = System.getProperty("server.port");
+        if (propPort != null) return Integer.parseInt(propPort);
+
+        // 2. ENV PORT
+        String envPort = System.getenv("PORT");
+        if (envPort != null) return Integer.parseInt(envPort);
+
+        // 3. Дефолт
+        return 8080;
     }
 }
